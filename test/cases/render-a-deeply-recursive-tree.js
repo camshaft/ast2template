cdc84f59574db7a60c73d4bd4c209897c8c721e8
camshaft <bytheway.cameron@gmail.com>
2015-01-10 23:05:47 -0700

    add a deep recursive test

---
+++ test/cases/render-a-deeply-recursive-tree.js
@@ -0,0 +1,36 @@
+function tree(i) {
+  if (i === 0) return [null, null];
+  var children = tree(i - 1);
+  return [
+    {
+      type: 'tag',
+      name: 'div',
+      children: [children[0]],
+      buffer: true
+    },
+    {
+      tag: 'div',
+      props: {},
+      children: children[1] ? [children[1]] : null
+    }
+  ];
+}
+
+var children = tree(500);
+
+exports.input = [
+  {
+    type: 'tag',
+    name: 'div',
+    children: [children[0]],
+    buffer: true
+  }
+];
+
+exports.output = {
+  tag: 'div',
+  props: {},
+  children: [children[1]]
+};
+
+exports.iterations = 1000;
