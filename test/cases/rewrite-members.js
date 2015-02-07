b5fa28ce71e484b98c9abccd98aaabb89d2543a1
camshaft <bytheway.cameron@gmail.com>
2015-02-06 17:11:56 -0700

    fix computed member traversal

---
+++ test/cases/rewrite-members.js
@@ -0,0 +1,43 @@
+exports.input = [
+  {
+    type: 'var',
+    expression: 'arr = [1,2,3,4,5,6]'
+  },
+  {
+    type: 'each',
+    expression: 'arr',
+    value: 'val',
+    key: 'key',
+    buffer: true,
+    children: [
+      {
+        type: 'tag',
+        name: 'div',
+        buffer: true,
+        children: [
+          {
+            type: 'expression',
+            expression: 'arr[key]',
+            buffer: true
+          },
+          {
+            type: 'expression',
+            expression: 'arr.length',
+            buffer: true
+          }
+        ]
+      }
+    ]
+  }
+];
+
+exports.output = [
+  { children: [ 1, 6 ], props: {}, tag: 'div' },
+  { children: [ 2, 6 ], props: {}, tag: 'div' },
+  { children: [ 3, 6 ], props: {}, tag: 'div' },
+  { children: [ 4, 6 ], props: {}, tag: 'div' },
+  { children: [ 5, 6 ], props: {}, tag: 'div' },
+  { children: [ 6, 6 ], props: {}, tag: 'div' }
+];
+
+exports.iterations = 100;
